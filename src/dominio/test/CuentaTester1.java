e874a5c7186700de326cafe4b06378fa8abacbb3
Pablo <pablof.a3f.4@gmail.com>
2019-04-12 14:05:04 +0200

    Creacion de clases Cuenta y Debito Tester
    Creacion de la clase CuentaTester y los metodos y javados de ingresar y
    retirar y la clase DebitoTester con su constructor y el metodo de setup
    y sus respectivos javadoc

---
+++ src/dominio/test/CuentaTester1.java
@@ -0,0 +1,28 @@
+package dominio.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Pruebas para la clase Cuenta
+ * @author pablo
+ *
+ */
+class CuentaTester1 extends TestCase{
+	
+	/**
+	 * Prueba para ingresar 1000 en la cuenta
+	 */
+	public void testIngresar1000()
+	{
+		
+	}
+	
+	/**
+	 * Prueba para retirar 1000 de la cuenta
+	 */
+	public void testRetirar1000()
+	{
+		
+	}
+
+}
---
+++ src/dominio/test/DebitoTester1.java
@@ -0,0 +1,43 @@
+package dominio.test;
+
+
+import java.util.Date;
+
+import dominio.Credito;
+import dominio.Cuenta;
+import dominio.Debito;
+import junit.framework.TestCase;
+
+/**
+ * Pruebas para la clase debito
+ * @author Pablo
+ *
+ */
+class DebitoTester1 extends TestCase{
+	Debito debito;
+	Cuenta cuenta;
+	
+	/**
+	 * Creacion del constructor de la clase
+	 * @param sTestName Nombre del test para la clase TestCase
+	 */
+	public DebitoTester1(String sTestName)
+	{
+		super(sTestName);
+	}
+	
+	/**
+	 * prueba de set para la clase de debito
+	 */
+	public void setUp() throws Exception
+	{
+		cuenta= new Cuenta("0001.0002.12.1234567890","Fulano de tal")
+		cuenta.ingresar(1000.0);
+		Date hoy = new Date();
+		long tiempo = Long.parseLong("12096000000");
+		Date fecha = new Date(hoy.getTime()+tiempo);
+		debito =new Debito("1234567890123456","Fulano de Tal",fecha);
+		debito.setCuenta(cuenta);
+	}
+	
+}
