fc5d1cb1c5baf7451d4efe8c5a2eb3ffd11eae74
Ruben <ruben.sanchez.carrasco@gmail.com>
2019-05-04 10:54:59 +0200

    Update Credito.java

--- src/dominio/Credito.java
+++ src/dominio/Credito.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.Vector;
 
 /**
- * Clase para hacer todo tipo de movimientos 
+ * Clase para hacer todo tipo de movimientos
  * relacionados con el credito de la tarjeta
  * @author Pablo
  *
@@ -13,14 +13,14 @@ public class Credito extends Tarjeta
 {
 	protected double mCredito;
 	protected Vector mMovimientos;
-	
-	
+
+
 	/**
-	 *constructor del Crédito
+	 *constructor del Crï¿½dito
 	 *@author Aketza
 	 *@param numero
-	 *@param titular titular del crédito
-	 *@param fechaCaducidad fecha de caducidad del crédito a conceder
+	 *@param titular titular del crï¿½dito
+	 *@param fechaCaducidad fecha de caducidad del crï¿½dito a conceder
 	 *@param credito cantidad a conceder
 	 */
 	public Credito(String numero,String titular,Date fechaCaducidad,double credito) {
@@ -28,30 +28,54 @@ public class Credito extends Tarjeta
 		mCredito=credito;
 		mMovimientos=new Vector();
 	}
-	
-	
+
+
 	/**
-	 * método para retirar el dinero del crédito
+	 * mï¿½todo para retirar el dinero del crï¿½dito
 	 * @author Aketza
 	 * @param x cantidad a retirar
-	 * @exception Excepción si tu saldo es insuficiente
+	 * @exception Excepciï¿½n si tu saldo es insuficiente
 	 * @return void
 	 */
 	public void retirar(double x) {
 		Movimiento m= new Movimiento();
-		m.setConcepto("Retirada en cajero automático");
-		x=(x*0.05<3.0? 3 : x*0.05); 
+		m.setConcepto("Retirada en cajero automï¿½tico");
+		x=(x*0.05<3.0? 3 : x*0.05);
 		m.setImporte(x);
 		mMovimientos.addElement(m);
-		
+
 		if (x>getCreditoDisponible())
-			throw new Exception("Crédito insuficiente");
-		
+			throw new Exception("Crï¿½dito insuficiente");
+
 	}
-	
-	
-	
-	
+
+	/**
+	* Metodo que sirve para ingresar dinero a la cuenta de credito asociada
+	* @author Ruben
+	* @param x cantidad a ingresar
+	*/
+	public void ingresar (double x) throws exception{
+		Movimiento m = new Movimiento();
+		m.setConcepto("Ingreso en cuenta asociada(cajero automÃ¡tico)");
+		m.setImporte(x);
+		mMovimientos.addElement(m);
+		mCuentaAsociada.ingresar(x);
+	}
+
+	/**
+	* Metodo que guarda los datos de un pago
+	* @author Ruben
+	* @param datos datos del establecimiento en el que se ha realizado el pago
+	* @param x cantidad ingresada
+	*/
+	public void pagoEnEstablecimiento(String datos, double x) throws exception{
+		Movimiento m=new Movimiento();
+		m.setConcepto("Compra a crÃ©dito en " + datos);
+		m.setImporte(x);
+		mMovimientos.addElement(m);
+	}
+
+
 	/**
 	 * Metodo que devuelve el saldo total
 	 * @return devuelve un numero con el saldo total
@@ -67,7 +91,7 @@ public class Credito extends Tarjeta
 		}
 		return r;
 	}
-	
+
 	/**
 	 * Metodo que devuelve el credito disponible
 	 * restandole el saldo al credito total
@@ -78,23 +102,23 @@ public class Credito extends Tarjeta
 	{
 		return mCredito-getSaldo();
 	}
-	
+
 	/**
 	 * Liquida todas las movimientos de la tarjeta de credito
-	 * clasificando el año y el mes para la misma
+	 * clasificando el aï¿½o y el mes para la misma
 	 * @param mes mes de la liquidacion
-	 * @param año año de la liquidacion
+	 * @param aï¿½o aï¿½o de la liquidacion
 	 * @author Pablo
 	 */
-	public void liquidar(int mes, int año)
+	public void liquidar(int mes, int aï¿½o)
 	{
 		Movimiento liq = new Movimiento();
-		liq.setConcepto("Liquidación de operaciones de tarj, crédito, " + (mes+1) + " de " + (año+1900));
+		liq.setConcepto("Liquidaciï¿½n de operaciones de tarj, crï¿½dito, " + (mes+1) + " de " + (aï¿½o+1900));
 		double r=0.0;
 		for(int=0;i<this.mMovimientos.size();i++)
 		{
 			Movimiento m= (Movimiento) mMovimientos.elementAt(i);
-			if (m.getFecha().getMonth()+1==mes && m.getFecha().getYear()+1900==año)
+			if (m.getFecha().getMonth()+1==mes && m.getFecha().getYear()+1900==aï¿½o)
 				r+=m.getImporte();
 		}
 		liq.setImporte(r);
