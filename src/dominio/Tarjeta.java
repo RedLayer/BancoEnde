3cd3fc2a5a6a12ac31cf5762c25ecb225cd1d0eb
Ruben <ruben.sanchez.carrasco@gmail.com>
2019-05-04 13:04:17 +0200

    Metodos añadidos a la clase Tarjeta
    Metodo constructor y setCuenta creados, además de los atributos, creados.

--- src/dominio/Tarjeta.java
+++ src/dominio/Tarjeta.java
@@ -1,14 +1,43 @@
 package dominio;
 
 /**
- * La tarjeta con sus respectivos campos 
+ * La tarjeta con sus respectivos campos
  * incluyendo objetos de otras clases
- * Al ser una clase abstracta todos sus hijos tendran que 
+ * Al ser una clase abstracta todos sus hijos tendran que
  * implementar las clases abstractas
  * @author Pablo
  *
  */
 public abstract class Tarjeta {
+
+	protected String mNumero, mTitular;
+	protected Date mFechaDeCaducidad;
+	/**
+	* Atributo que guarda la referencia a la cuenta bancaria asociada a la Tarjeta
+	*/
+	protected Cuenta mCuentaAsociada;
+
+	/**
+	* Constructor de la clase Tarjeta
+	* @author Ruben
+	* @param numero numero identificativo de la Tarjeta
+	* @param titular nombre del titular de la Tarjeta
+	* @param fechaCaducidad fecha en la que la tarjeta dejar√° de ser v√°lida
+	*/
+	public Tarjeta(String numero, String titular, Date fechaCaducidad){
+		mNumero=numero;
+		mTitular=titular;
+		mFechaDeCaducidad=fechaCaducidad;
+	}
+
+	/**
+	* Metodo que asocia una cuenta bancaria a una Tarjeta
+	* @author Ruben
+	* @param c cuenta bancaria que se asociar√° a la Tarjeta
+	*/
+	public void setCuenta(Cuenta c){
+		mCuentaAsociada=c;
+	}
 
 	/**
 	 * Metodo abstracto sobre el pago en establecimiento
@@ -18,28 +47,28 @@ public abstract class Tarjeta {
 	 * @author Pablo
 	 */
 	public abstract void pagoEnEstablecimiento(String datos, double x) throws Exception;
-	
+
 	/**
-	 *MÈtodo abstracto sobre la retirada de saldo
+	 *MÔøΩtodo abstracto sobre la retirada de saldo
 	 *@author Aketza
 	 *@param x dinero a retirar
 	 *@return void
 	 */
 	public abstract void retirar(double x);
-	
+
 	/**
-	 *MÈtodo abstracto sobre el ingreso de dinero
+	 *MÔøΩtodo abstracto sobre el ingreso de dinero
 	 *@author Aketza
 	 *@param x dinero a ingresar
 	 *@return void
 	 */
 	public abstract void ingresar(double x);
-	
-	
+
+
 	/**
 	 * Metodo abstracto para recoger el saldo
 	 * @return devuelve un numero con el saldo
 	 * @Author Pablo
-	 */ 
+	 */
 	public abstract double getSaldo();
 }
